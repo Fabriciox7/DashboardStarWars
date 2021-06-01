7ce872305a7f0dc752c6a57fcb719ce0297876f3
Fabricio Moura <fabricioldi36@alu.ufc.br>
2021-06-01 11:54:12 -0300

    primeiras requisições

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-// Make a request for a user with a given ID
-axios
-  .get('https://swapi.dev/api/people/1/')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
+//document.getElementById('personagens').innerHTML = 22;
+
+const personagensContador = document.getElementById('personagens');
+const luasContador = document.getElementById('luas');
+const planetasContador = document.getElementById('planetas');
+const navesContador = document.getElementById('naves');
+
+preencherContadores();
+
+function preencherContadores() {
+  Promise.all([
+    swapiGet('people/'),
+    swapiGet('vehicles/'),
+    swapiGet('planets/'),
+    swapiGet('starships/'),
+  ]).then(function (results) {
+    personagensContador.innerHTML = results[0].data.count;
+    luasContador.innerHTML = results[1].data.count;
+    planetasContador.innerHTML = results[2].data.count;
+    navesContador.innerHTML = results[3].data.count;
   });
+}
+
+function swapiGet(param) {
+  return axios.get(`https://swapi.dev/api/${param}`);
+}
